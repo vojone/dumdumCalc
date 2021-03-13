af793edd909b4aae9f5cc20341d7762028104209
Vojtech Dvorak <vvd1@centrum.cz>
2021-03-13 11:10:15 +0100

    Added other math functions to math_lib.h and created math_lib.cpp for their implementation
    Added decleration of other expected functions (^,root,abs,!) and created math_lib.cpp
    (empty only with header), it was necessity for tests

---
+++ src/math_lib.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *                            dumdumCalculator       
+ *                              math_lib.cpp
+ * 
+ *       Purpose: Implementation of mathematical library used in calculator
+ * 
+ *        Authors: Radek Marek, Vojtech Dvorak, Tomas Dvorak, Juraj Dedic
+ *                       Last change: 13. 3. 2021
+ *   
+ * ***************************************************************************/
+
+/**
+ * @file math_lib.cpp
+ * 
+ * @brief Source file (implementation) of mathematical lib used in calculator
+ * @authors Radek Marek, Vojtech Dvorak, Tomas Dvorak, Juraj Dedic
+ */
+
+#include "math_lib.h"
+
+/***                          End of math_lib.cpp                          ***/
--- src/math_lib.h
+++ src/math_lib.h
@@ -2,10 +2,10 @@
  *                            dumdumCalculator       
  *                               math_lib.h
  * 
- *       Description: Header file of mathematical library for calculator
+ *        Purpose: Header file of mathematical library for calculator
  * 
  *        Authors: Radek Marek, Vojtech Dvorak, Tomas Dvorak, Juraj Dedic
- *                       Last change: 12. 3. 2021
+ *                       Last change: 13. 3. 2021
  *   
  * ***************************************************************************/
 
@@ -19,39 +19,67 @@
 #ifndef __MATH_LIB__
 #define __MATH_LIB__
 
-/*****************************ELEMENTARY FUNCTIONS****************************/
-
 /**
- * @brief Arithmetical addition of two doubles
+ * @brief Arithmetic addition of two doubles
  * @param num1 first number to add
  * @param num2 second number to add
  * @return result of addition
  */
-extern double add(double num1, double num2);
+extern double add(const double num1, const double num2);
 
 /**
- * @brief Arithmetical subtraction of two doubles (num1 - num2)
+ * @brief Arithmetic subtraction of two doubles (num1 - num2)
  * @param num1 minuend
  * @param num2 subtrahend
  * @return difference of num1 and num2
  */
-extern double sub(double num1, double num2);
+extern double sub(const double num1, const double num2);
 
 /**
- * @brief Arithmetical multiplication of two doubles
+ * @brief Arithmetic multiplication of two doubles
  * @param num1 first number to multiply
  * @param num2 second number to multiply
  * @return result of multiplication
  */
-extern double mult(double num1, double num2);
+extern double mult(const double num1, const double num2);
 
 /**
- * @brief Arithmetical division of two doubles
+ * @brief Arithmetic division of two doubles
  * @param num1 divident
  * @param num2 divisor
  * @return result of division
  */
-extern double div(double num1, double num2);
+extern double div(const double num1, const double num2);
+
+/**
+ * @brief Product of all integers less or equal to num (factorial)
+ * @param num number, from which is factorial made
+ * @return factorial numbers from parameter
+ */
+extern long int fact(const int num);
+
+/**
+ * @brief Exponetiation with real base and natural exponent
+ * @param base (real) number which is base for exponetiation
+ * @param exp natural exponent
+ * @return result of exponentiation
+ */
+extern double pow(const double base, const unsigned int exp);
+
+/**
+ * @brief Calculates general root of a base number 
+ * @param base (real) number from which is found root
+ * @param exp natural number which specifies the root
+ * @return root of number in argument
+ */
+extern double root(const double base, const unsigned int exp);
+
+/**
+ * @brief Calculates the absolute value of a number in argument
+ * @param num number from which is calculated the absolute value
+ * @return absolute value of number 
+ */
+extern double fabs(const double num);
 
 #endif //__MATH_LIB__
 
