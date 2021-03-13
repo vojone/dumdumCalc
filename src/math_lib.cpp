899756a4b4a455fa55e0c910869bcc52472ea245
Vojtech Dvorak <vvd1@centrum.cz>
2021-03-13 12:38:57 +0100

    Added empty defs to math_lib.cpp for building binary with tests

--- src/math_lib.cpp
+++ src/math_lib.cpp
@@ -18,4 +18,82 @@
 
 #include "math_lib.h"
 
+/**
+ * @brief Arithmetic addition of two doubles
+ * @param num1 first number to add
+ * @param num2 second number to add
+ * @return result of addition
+ */
+double add(const double num1, const double num2) {
+  return 0.0;
+}
+
+/**
+ * @brief Arithmetic subtraction of two doubles (num1 - num2)
+ * @param num1 minuend
+ * @param num2 subtrahend
+ * @return difference of num1 and num2
+ */
+double sub(const double num1, const double num2) {
+  return 0.0;
+}
+
+/**
+ * @brief Arithmetic multiplication of two doubles
+ * @param num1 first number to multiply
+ * @param num2 second number to multiply
+ * @return result of multiplication
+ */
+double mult(const double num1, const double num2) {
+  return 0.0;
+}
+
+/**
+ * @brief Arithmetic division of two doubles
+ * @param num1 divident
+ * @param num2 divisor
+ * @return result of division
+ */
+double div(const double num1, const double num2) {
+  return 0.0;
+}
+
+/**
+ * @brief Product of all integers less or equal to num (factorial)
+ * @param num number, from which is factorial made
+ * @return factorial numbers from parameter
+ */
+long int fact(const int num) {
+  return 0;
+}
+
+/**
+ * @brief Exponetiation with real base and natural exponent
+ * @param base (real) number which is base for exponetiation
+ * @param exp natural exponent
+ * @return result of exponentiation
+ */
+double pow(const double base, const unsigned int exp) {
+  return 0.0;
+}
+
+/**
+ * @brief Calculates general root of a base number 
+ * @param base (real) number from which is found root
+ * @param exp natural number which specifies the root
+ * @return root of number in argument
+ */
+double root(const double base, const unsigned int exp) {
+  return 0.0;
+}
+
+/**
+ * @brief Calculates the absolute value of a number in argument
+ * @param num number from which is calculated the absolute value
+ * @return absolute value of number 
+ */
+double fabs(const double num) {
+  return 0.0;
+}
+
 /***                          End of math_lib.cpp                          ***/
