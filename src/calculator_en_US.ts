5216aa9f9554b4d0ced5fdcd33fa9fc8f27a0e2f
Juraj Dedič <xdedic07@vutbr.cz>
2021-04-01 17:32:41 +0200

    UI components placing
    Added all components (pushButtons, text) to the mainwindow
    modified .gitignore to not include debug files generated by QT

--- src/calculator_en_US.ts
+++ src/calculator_en_US.ts
@@ -1,3 +1,93 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="calculator_en_US"></TS>
+<TS version="2.1" language="calculator_en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="294"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="44"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="295"/>
+        <source>%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="296"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="86"/>
+        <location filename="mainwindow.ui" line="149"/>
+        <location filename="mainwindow.ui" line="170"/>
+        <location filename="mainwindow.ui" line="191"/>
+        <location filename="mainwindow.ui" line="212"/>
+        <location filename="mainwindow.ui" line="233"/>
+        <location filename="mainwindow.ui" line="275"/>
+        <location filename="mainwindow.ui" line="296"/>
+        <location filename="mainwindow.ui" line="317"/>
+        <location filename="mainwindow.ui" line="338"/>
+        <location filename="mainwindow.ui" line="398"/>
+        <location filename="mainwindow.ui" line="419"/>
+        <location filename="mainwindow.ui" line="440"/>
+        <location filename="mainwindow.ui" line="461"/>
+        <location filename="mainwindow.ui" line="503"/>
+        <location filename="mainwindow.ui" line="524"/>
+        <location filename="mainwindow.ui" line="545"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="297"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="300"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="301"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="302"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="303"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="304"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="306"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="307"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="308"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="309"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="311"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="312"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="313"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="314"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="316"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="317"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="318"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="107"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="298"/>
+        <source>^</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="128"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="299"/>
+        <source>/</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="254"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="305"/>
+        <source>&lt;-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="362"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="310"/>
+        <source>=</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="482"/>
+        <location filename="build-calculator-Desktop_x86_windows_msvc2017_pe_32bit-Debug/calculator_autogen/include/ui_mainwindow.h" line="315"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
